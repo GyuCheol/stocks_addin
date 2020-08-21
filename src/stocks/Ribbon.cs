c7446d175fbdb88648acb092791990106c9bc13c
GyuCheol <rbcjf0219@gmail.com>
2020-08-22 02:12:38 +0900

    타이머 코드 변경 (BackgroundWorker)

--- src/stocks/Ribbon.cs
+++ src/stocks/Ribbon.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
@@ -16,26 +18,39 @@ namespace stocks
     {
         private const string FIELDS = "Ask,AskSize,AverageDailyVolume10Day,AverageDailyVolume3Month,Bid,BidSize,BookValue,Currency,DividendDate,EarningsTimestamp,EarningsTimestampEnd,EarningsTimestampStart,EpsForward,EpsTrailingTwelveMonths,Exchange,ExchangeDataDelayedBy,ExchangeTimezoneName,ExchangeTimezoneShortName,FiftyDayAverage,FiftyDayAverageChange,FiftyDayAverageChangePercent,FiftyTwoWeekHigh,FiftyTwoWeekHighChange,FiftyTwoWeekHighChangePercent,FiftyTwoWeekLow,FiftyTwoWeekLowChange,FiftyTwoWeekLowChangePercent,FinancialCurrency,ForwardPE,FullExchangeName,GmtOffSetMilliseconds,Language,LongName,Market,MarketCap,MarketState,MessageBoardId,PriceHint,PriceToBook,QuoteSourceName,QuoteType,RegularMarketChange,RegularMarketChangePercent,RegularMarketDayHigh,RegularMarketDayLow,RegularMarketOpen,RegularMarketPreviousClose,RegularMarketPrice,RegularMarketTime,RegularMarketVolume,PostMarketChange,PostMarketChangePercent,PostMarketPrice,PostMarketTime,SharesOutstanding,ShortName,SourceInterval,Symbol,Tradeable,TrailingAnnualDividendRate,TrailingAnnualDividendYield,TrailingPE,TwoHundredDayAverage,TwoHundredDayAverageChange,TwoHundredDayAverageChangePercent";
 
-        private Timer timer = new Timer();
+        private BackgroundWorker worker = new BackgroundWorker();
 
         public Microsoft.Office.Interop.Excel.Application App => Globals.ThisAddIn.Application;
 
+
         private void Ribbon_Load(object sender, RibbonUIEventArgs e)
         {
-            timer.Tick += Timer_Tick;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            timer.Stop();
-            RefreashTableTaskAsync();
+            if (toggleButton1.Checked)
+            {
+                // 최소 0.5초
+                if (int.TryParse(editBox1.Text, out int result))
+                {
+                    Thread.Sleep(result);
+                    worker.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            RefreashTableTask();
         }
 
         private void button1_Click(object sender, RibbonControlEventArgs e)
         {
             Workbook wb = App.ActiveWorkbook;
-            Worksheet ws = App.ActiveSheet;
-            Range cell = App.ActiveCell;
 
             if (wb == null)
             {
@@ -43,6 +58,7 @@ namespace stocks
                 return;
             }
 
+            Worksheet ws = App.ActiveSheet;
             ListObject table = ws.ListObjects.AddEx(XlListObjectSourceType.xlSrcRange, XlListObjectHasHeaders: XlYesNoGuess.xlNo);
             table.Name = table.Name + "_stock";
             table.Range[1, 1] = "ticker";
@@ -66,61 +82,61 @@ namespace stocks
                 return;
             }
 
-            RefreashTableTaskAsync();
+            worker.RunWorkerAsync();
         }
 
-        public async void RefreashTableTaskAsync()
+        public void RefreashTableTask()
         {
             Workbook wb = App.ActiveWorkbook;
 
-            // 모든 업데이트 테이블 가져오기
-            List<ListObject> tables = new List<ListObject>();
-
-            foreach (Worksheet ws in wb.Worksheets)
+            try
             {
-                foreach (ListObject table in ws.ListObjects)
+                // 모든 업데이트 테이블 가져오기
+                List<ListObject> tables = new List<ListObject>();
+
+                foreach (Worksheet ws in wb.Worksheets)
                 {
-                    if (table.Name.EndsWith("_stock"))
+                    foreach (ListObject table in ws.ListObjects)
                     {
-                        tables.Add(table);
-                    }
-                }
-            }
-
-            // table의 param 가져오기
-            List<string> tickers = new List<string>();
-            List<Field> fields = new List<Field>();
-
-            foreach (ListObject table in tables)
-            {
-
-                for (int r = 2; r <= 1 + table.ListRows.Count; r++)
-                {
-                    string text = table.Range[r, 1].Text;
-
-                    if (tickers.Contains(text) == false)
-                    {
-                        tickers.Add(text);
-                    }
-                }
-
-                for (int c = 2; c <= table.ListColumns.Count; c++)
-                {
-                    if (Enum.TryParse(table.Range[1, c].Text, out Field field))
-                    {
-                        if (fields.Contains(field) == false)
+                        if (table.Name.EndsWith("_stock"))
                         {
-                            fields.Add(field);
+                            tables.Add(table);
                         }
                     }
                 }
-            }
 
-            // yahoo finance 정보 가져오기 (비동기)
-            var securities = await Yahoo.Symbols(tickers.ToArray()).Fields(fields.ToArray()).QueryAsync();
+                // table의 param 가져오기
+                List<string> tickers = new List<string>();
+                List<Field> fields = new List<Field>();
+
+                foreach (ListObject table in tables)
+                {
+
+                    for (int r = 2; r <= 1 + table.ListRows.Count; r++)
+                    {
+                        string text = table.Range[r, 1].Text;
+
+                        if (tickers.Contains(text) == false)
+                        {
+                            tickers.Add(text);
+                        }
+                    }
+
+                    for (int c = 2; c <= table.ListColumns.Count; c++)
+                    {
+                        if (Enum.TryParse(table.Range[1, c].Text, out Field field))
+                        {
+                            if (fields.Contains(field) == false)
+                            {
+                                fields.Add(field);
+                            }
+                        }
+                    }
+                }
+
+                // yahoo finance 정보 가져오기 (비동기)
+                var securities = Yahoo.Symbols(tickers.ToArray()).Fields(fields.ToArray()).QueryAsync().Result;
             
-            try
-            {
                 foreach (ListObject table in tables)
                 {
                     object[,] values = new object[table.ListRows.Count, table.ListColumns.Count];
@@ -166,30 +182,18 @@ namespace stocks
                 // 셀 값을 입력할 수 없는 타이밍이 때 걸린 경우.
                 Console.Write(ex.Message);
             }
-
-            toggleButton1_Click(null, null);
+            
         }
 
         private void toggleButton1_Click(object sender, RibbonControlEventArgs e)
         {
-            if (this.toggleButton1.Checked)
+            if (toggleButton1.Checked)
             {
-                // 최소 0.5초
-                if (int.TryParse(editBox1.Text, out int result))
-                {
-                    timer.Interval = Math.Max(result, 500);
-                }
-                else
-                {
-                    timer.Interval = 1000;
-                }
-
-                editBox1.Text = timer.Interval.ToString();
-                timer.Start();
+                worker.RunWorkerAsync();
             }
             else
             {
-                timer.Stop();
+                worker.CancelAsync();
             }
         }
     }
